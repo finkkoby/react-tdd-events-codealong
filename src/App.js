4bd0b25c727557286e197a660923ede6834ce243
Koby Fink <finkkoby@icloud.com>
2024-03-01 17:43:11 -0500

    complete codealong

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { useState } from "react";
+
 function App() {
+  const [pepperoni, setPepperoni] = useState(false)
+
+  function handlePepproniChange() {
+    setPepperoni(!pepperoni)
+  }
+
   return (
-    <div></div>
-  )
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={pepperoni}
+        aria-checked={false}
+        onChange={handlePepproniChange}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoni ? <li>Pepperoni</li> : null}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
